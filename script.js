4f885f48d3ea113ba208144b97af2d665622d755
real.ilia.dzneladze@gmail.com <real.ilia.dzneladze@gmail.com>
2025-10-05 16:32:47 +0200

    Finished the script for game

--- script.js
+++ script.js
@@ -9,9 +9,43 @@ function getComputerChoice(){
 
 function getHumanChoice(){
     // prompt the user for their choice
-    const input = prompt("Your Choice: ")
+    let input = prompt("Your Choice: ")
     // put to lower case to standardize and match
     // existing array
-    input = input.toLowerCase
+    input = input.toLowerCase()
     return input
+}
+
+let humanScore = 0, computerScore = 0
+
+function playRound(humanChoice, computerChoice){
+    if(humanChoice === computerChoice){
+        console.log("Tie!");
+    } else if (
+        (humanChoice == "rock" && computerChoice == "paper") ||
+        (humanChoice == "paper" && computerChoice == "scissors") ||
+        (humanChoice == "scissors" && computerChoice == "rock")){
+        console.log(`You Lose! ${computerChoice} beats ${humanChoice}`)
+        computerScore++
+    } else {
+        console.log(`You Win! ${humanChoice} beats ${computerChoice}`)
+        humanScore++
+    }
+}
+
+function playGame(){
+    for (let i = 0; i < 5; i++) {
+        playRound(getHumanChoice(),getComputerChoice())
+    }
+}
+
+playGame()
+
+console.log(`Human: ${humanScore}, Computer: ${computerScore}`)
+if(humanScore > computerScore){
+    console.log("Human Wins!!!")
+} else if (humanScore = computerScore){
+    console.log("Tie!")
+} else{
+    console.log("Computer Wins!")
 }
